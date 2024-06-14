cc9c3f5e807f1fb2d9e2be304d3fdaf1261d1a33
Utkarsh Pal <93117781+UtkarshPal45@users.noreply.github.com>
2024-06-14 23:04:16 +0530

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -8,7 +8,7 @@ export const SocketContextProvider = ({children})=>{
     const { currentUser } = useContext(AuthContext);
 
     useEffect(()=>{
-       setSocket(io("http://localhost:4000"))
+       setSocket(io("https://real-estate-socketserver.onrender.com"))
     },[])
 
     useEffect(() => {
